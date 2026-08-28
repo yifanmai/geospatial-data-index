d51cddb0f9bf3b3c8a2ce972e3bc3046b5fe48b4
Yifan Mai <yifan@cs.stanford.edu>
2026-08-28 12:38:20 -0700

    Fix types and refactor out adding derived_from link

--- src/geospatial_data_index/crawl.py
+++ src/geospatial_data_index/crawl.py
@@ -36,16 +36,20 @@ def get_crawl_status(catalog_destination_directory: Path) -> str:
         return NOT_STARTED
 
 
-def set_crawl_status(catalog_destination_directory: Path, status: str) -> str:
+def set_crawl_status(catalog_destination_directory: Path, status: str) -> None:
     catalog_destination_directory.mkdir(parents=True, exist_ok=True)
     status_destination_path = catalog_destination_directory / STATUS_FILE_NAME
     with open(status_destination_path, "w") as status_file:
         json.dump({STATUS_KEY: status}, status_file)
 
 
-def add_derived_from_link(stac_object: STACObject):
+def add_derived_from_link_to_self_href(stac_object: STACObject) -> None:
+    # See https://github.com/radiantearth/stac-spec/blob/v1.1.0/best-practices.md#using-relation-types
+    # for "derived_from" link
+    self_href = stac_object.get_self_href()
+    assert self_href
     if not stac_object.get_single_link(DERIVED_FROM):
-        stac_object.add_link(Link(rel=DERIVED_FROM, target=stac_object.get_self_href(), media_type="application/json"))
+        stac_object.add_link(Link(rel=DERIVED_FROM, target=self_href, media_type="application/json"))
 
 
 def crawl_root_catalog(source_path: str, destination_path: Path, id_strategy: str = USE_ID):
@@ -66,10 +70,7 @@ def normalize_and_save_catalog(catalog: Catalog, destination_path: Path, id_stra
         catalog_destination_path = catalog_destination_directory / "collection.json"
     else:
         catalog_destination_path = catalog_destination_directory / "catalog.json"
-    # See https://github.com/radiantearth/stac-spec/blob/v1.1.0/best-practices.md#using-relation-types
-    # for "derived_from" link
-    if not catalog.get_single_link(DERIVED_FROM):
-        catalog.add_link(Link(rel=DERIVED_FROM, target=catalog.get_self_href(), media_type="application/json"))
+    add_derived_from_link_to_self_href(catalog)
     catalog.set_self_href(str(catalog_destination_path))
     for child in catalog.get_children():
         try:
@@ -79,23 +80,26 @@ def normalize_and_save_catalog(catalog: Catalog, destination_path: Path, id_stra
     if isinstance(catalog, Collection):
         for item in catalog.get_items():
             try:
-                normalize_and_save_item(item, catalog_destination_directory, id_strategy)
+                normalize_and_save_item(item, catalog, catalog_destination_directory, id_strategy)
             except Exception as e:
                 logger.exception(f"Could not save {item.id} to {catalog_destination_directory}")
     set_crawl_status(catalog_destination_directory, FINISHED)
     catalog.save_object()
 
 
-def normalize_and_save_item(item: Item, collection_destination_directory: Path, id_strategy: str = USE_ID) -> None:
+def normalize_and_save_item(item: Item, collection: Collection, collection_destination_directory: Path, id_strategy: str = USE_ID) -> None:
     sleep(0.1)
     collection_destination_directory.mkdir(parents=True, exist_ok=True)
     item_destination_path = collection_destination_directory / f"{item.id}.json"
+    collection_href = collection.get_self_href()
+    assert collection_href
     collection_links = item.get_links("collection")
     if len(collection_links) > 1:
         raise Exception("Expected at most one collection link")
     elif len(collection_links) == 1:
-        collection_links[0].target = item.get_parent().get_self_href()
-    if not item.get_single_link(DERIVED_FROM):
-        item.add_link(Link(rel=DERIVED_FROM, target=item.get_self_href(), media_type="application/json"))
+        collection_links[0].target = collection_href
+    elif len(collection_links) == 0:
+        item.add_link(Link.parent(collection))
+    add_derived_from_link_to_self_href(item)
     item.set_self_href(str(item_destination_path))
     item.save_object()
